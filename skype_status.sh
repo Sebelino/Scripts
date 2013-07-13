037eda29b3bcb039acd55a835877f8b181afcdbf
Sebelino <sebelino@hotmail.com>
2013-07-14 00:10:17 +0200

    Wrote a script for checking the status of Skype.

--- skype_status.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Displays a short description of the status of Skype (notifications, etc.).
-
-echo "<fc=#AA44EE>"$(cat ~/.xmonad/xmobar_data/skype_status)"</fc>"
---
+++ start_daemons.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python2 ./daemons/skype_status.py &
+
