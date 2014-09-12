d425e2f11e3c2babb2df92d256d9ddda486116bd
Sebastian Olsson <sebelino@hotmail.com>
2014-09-12 20:49:44 +0200

    Minor adjustments.

--- start_daemons.sh
+++ start_daemons.sh
@@ -3,5 +3,5 @@
 #SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 #python2 "$SCRIPTPATH/daemons/skype_status.py" &
 
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-python2 "$SCRIPTPATH/daemons/instantboss2/instantboss2.py" 30 xmobar &
+#SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+#python2 "$SCRIPTPATH/daemons/instantboss2/instantboss2.py" 30 xmobar &
